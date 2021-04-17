df70b4b83e05105c15f20dc6cc61f1463411b2a6
Patricio Merino <OmniSliver@users.noreply.github.com>
2021-04-17 03:42:27 -0400

    Replace template data with the real action (#1)
    * Replace template data with the real action
    
    * npm run all
    
    * npm install --save-dev eslint-config-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
    
    * Return the whole stringified error
    
    * Compiled
    
    * console.log the error
    
    * log github and core keys
    
    * Add cache to the test action
    
    * Add core debug messages
    
    * Stringified debug variables
    
    * Fix PR base
    
    * Fix octokit.repos.compareCommits result structure
    
    * Fix no patch
    
    * Single quote outputs
    
    * Fix getLineNumbers
    
    * Exec git fetch
    
    * Don't use the cheout action
    
    * Test with branch realAction instead of local action
    
    * Re use the local action
    
    * Separate ref parsing into a new function: getRefs
    
    * git fetch from context repo
    
    * git fetch from origin
    
    * Use git diff, use include and ignore RegExps
    
    * core.error execAsync's errors
    
    * Don't fail git fetch on errors
    
    * Fix args
    
    * Fix for ambiguous argument 'main...ef6ee51962d18d59fb79fab7dc74e7f4674eff10': unknown revision or path not in the working tree. in getPaths
    
    * Add -- to git diff in getPaths
    
    * Change base ref to the sha

--- README.md
+++ README.md
@@ -1,116 +1,55 @@
-# Create a JavaScript Action
+# Diff line numbers javascript action
 
-<p align="center">
-  <a href="https://github.com/actions/javascript-action/actions"><img alt="javscript-action status" src="https://github.com/actions/javascript-action/workflows/units-test/badge.svg"></a>
-</p>
+This action outputs the line number of the deleted/added lines of modified or added files.
 
-Use this template to bootstrap the creation of a JavaScript action.:rocket:
+The line numbers are obtained by parsing the patch chunks of each file given by `git diff`
 
-This template includes tests, linting, a validation workflow, publishing, and versioning guidance.
+NOTE: Requires having used `actions/checkout@v2` in a previous step.
 
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
+## Inputs
 
-## Create an action from this template
+### `include`
 
-Click the `Use this Template` and provide the new repo details for your action
+**Optional** JSON array. Only process paths that match a regular expression in `include`. By default includes all.
 
-## Code in Main
+### `ignore`
 
-Install the dependencies
+**Optional** JSON array. Do not process paths that match a regular expression in `ignore`. By default ignores none.
 
-```bash
-npm install
-```
+## Outputs
 
-Run the tests :heavy_check_mark:
+### `lineNumbers`
 
-```bash
-$ npm test
-
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
-...
-```
-
-## Change action.yml
-
-The action.yml defines the inputs and output for your action.
-
-Update the action.yml with your name, description, inputs and outputs for your action.
-
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
-
-## Change the Code
-
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
-
+An array with the files' path, added lines and removed lines. It looks like:
 ```javascript
-const core = require('@actions/core');
-...
-
-async function run() {
-  try {
-      ...
-  }
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run()
+[
+  {
+    path: string,
+    added: number[],
+    removed: number[]
+  },
+  ...
+]
 ```
+- `path` is the file's path, e.g. `package.json` and `src/index.js`.
+- `added` is an array of numbers. Each number is the line number of an added line.
+- `removed` is an array of numbers. Each number is the line number of a removed line.
 
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
+## Example usage
 
-## Package for distribution
-
-GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
-
-Actions are run from GitHub repos.  Packaging the action will create a packaged action in the dist folder.
-
-Run prepare
-
-```bash
-npm run prepare
+```yml
+name: example
+on: [pull_request]
+jobs:
+  example:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Get diff lines
+        id: diff
+        uses: Equip-Collaboration/diff-line-numbers@v1
+        with:
+          include: '["\\.js$", "\\.jsx$"]'
+          ignore: '["^dist/", "^bin/", "^www/"]'
+      - name: Print line numbers of changed lines
+        run: echo Line numbers = ${{ toJSON(steps.diff.outputs.lineNumbers) }}
 ```
-
-Since the packaged index.js is run from the dist folder.
-
-```bash
-git add dist
-```
-
-## Create a release branch
-
-Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
-
-Checkin to the v1 release branch
-
-```bash
-git checkout -b v1
-git commit -a -m "v1 release"
-```
-
-```bash
-git push origin v1
-```
-
-Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
-
-Your action is now published! :rocket:
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Usage
-
-You can now consume the action by referencing the v1 branch
-
-```yaml
-uses: actions/javascript-action@v1
-with:
-  milliseconds: 1000
-```
-
-See the [actions tab](https://github.com/actions/javascript-action/actions) for runs of this action! :rocket:
--- index.js
+++ index.js
@@ -1,21 +1,292 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
 
+run()
 
-// most @actions toolkit packages have async methods
+/**
+ * Sets the variable lineNumbers in the action's output
+ */
 async function run() {
+  // https://github.com/actions/toolkit/blob/main/docs/action-debugging.md#how-to-access-step-debug-logs
+  core.debug('run: Starting...')
+
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const include = core.getInput('include')
+    const ignore = core.getInput('ignore')
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.debug(`run: include=${JSON.stringify(include, null, 2)}`)
+    core.debug(`run: ignore=${JSON.stringify(ignore, null, 2)}`)
 
-    core.setOutput('time', new Date().toTimeString());
+    const { base, head } = getRefs()
+
+    core.debug(`run: base=${JSON.stringify(base, null, 2)}`)
+    core.debug(`run: head=${JSON.stringify(head, null, 2)}`)
+
+    core.debug(`run: fetching ${base}...`)
+
+    await execAsync('git', ['fetch', 'origin', base], { failOnStdErr: false })
+
+    core.debug(`run: ${base} fetched`)
+
+    const paths = await getPaths(
+      base,
+      head,
+      JSON.parse(include),
+      JSON.parse(ignore)
+    )
+
+    core.debug(`run: paths=${JSON.stringify(paths, null, 2)}`)
+
+    const diffs = await getDiffs(base, head, paths)
+
+    core.startGroup('run: diffs')
+    core.debug(`run: diffs=${JSON.stringify(diffs, null, 2)}`)
+    core.endGroup()
+
+    const lineNumbers = diffs.map(diff => getLineNumbers(diff))
+
+    core.startGroup('run: lineNumbers')
+    core.debug(`run: lineNumbers=${JSON.stringify(lineNumbers, null, 2)}`)
+    core.endGroup()
+
+    const output = []
+    for (let i = 0; i < paths.length; i++) {
+      output.push({
+        path: paths[i],
+        added: lineNumbers[i].added,
+        removed: lineNumbers[i].removed
+      })
+    }
+
+    core.startGroup('run: output')
+    core.debug(`run: output=${JSON.stringify(output, null, 2)}`)
+    core.endGroup()
+
+    core.setOutput('lineNumbers', output)
   } catch (error) {
-    core.setFailed(error.message);
+    core.error(error)
+
+    core.setFailed(error.message)
   }
+  core.debug('run: Ended')
 }
 
-run();
+/**
+ * Gets the diffs (one for each file) between `base` and `head`
+ *
+ * @param {string} base The git ref to compare from
+ * @param {string} head The git ref to compare to
+ * @param {string[]} paths The paths of the files to compare
+ * @returns {string[]} Diff patches
+ */
+async function getDiffs(base, head, paths) {
+  core.startGroup('getDiffs')
+
+  const diffs = []
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i]
+
+    const { stdout } = await execAsync('git', [
+      'diff',
+      '-U0',
+      `--minimal`,
+      `--diff-filter=ad`,
+      `--inter-hunk-context=0`,
+      `-w`,
+      `${base}`,
+      '--',
+      path
+    ])
+
+    core.debug(`getDiffs: stdout=${stdout}`)
+
+    diffs.push(stdout)
+  }
+
+  core.endGroup()
+
+  return diffs
+}
+
+/**
+ * Gets the path of added or modified (ignores whitespace) files between `base`
+ * and `head`.
+ *
+ * - Only return paths that match a regular expression in `include`. By default
+ * includes all.
+ * - Do not return paths that match a regular expression in `ignore`. By
+ * default ignores none.
+ *
+ * @param {string} base The git ref to compare from
+ * @param {string} head The git ref to compare to
+ * @param {string[]} include A list of regular expressions
+ * @param {string[]} ignore A list of regular expressions
+ * @returns {string[]} The paths
+ */
+async function getPaths(base, head, include = [''], ignore = []) {
+  const includeRegexps = include.map(re => new RegExp(re))
+
+  core.debug(
+    `getPaths: includeRegexps=${JSON.stringify(
+      includeRegexps.map(re => re.toString())
+    )}`
+  )
+
+  const ignoreRegexps = ignore.map(re => new RegExp(re))
+
+  core.debug(
+    `getPaths: ignoreRegexps=${JSON.stringify(
+      ignoreRegexps.map(re => re.toString())
+    )}`
+  )
+
+  const { stdout } = await execAsync('git', [
+    'diff',
+    '--name-only',
+    `--diff-filter=ad`,
+    `-w`,
+    `${base}`,
+    `--`
+  ])
+
+  core.debug(`getPaths: stdout=${stdout}`)
+
+  return stdout
+    .split('\n')
+    .filter(
+      path =>
+        path &&
+        includeRegexps.some(re => re.test(path)) &&
+        !ignoreRegexps.some(re => re.test(path))
+    )
+}
+
+// Matches a patch chunk line and captures the chunk's numbers.
+// E.g.: Matches "@@ -27,7 +198,6 @@ ..." and captures 27, 7, 198 and 6
+// E.g.: Matches "@@ -27 +198,0 @@ ..." and captures 27, undefined, 198 and 0
+// Capture groups:             |-1-|    |-2-|     |-3-|    |-4-|
+const patchLinesRegexp = /^@@ -(\d+)(?:,(\d+))? \+(\d+)(?:,(\d+))? @@/
+
+/**
+ * Gets the line numbers (removed and added) from the patch
+ *
+ * @param {string} patch The diff's patch
+ * @returns {object} { added: number[], removed: number[] }
+ */
+function getLineNumbers(patch) {
+  const removed = []
+  const added = []
+
+  const patchLines = patch ? patch.split('\n') : []
+  for (let i = 0; i < patchLines.length; i++) {
+    const match = patchLines[i].match(patchLinesRegexp)
+    if (match) {
+      const remStart = Number.parseInt(match[1])
+      const remNumber = match[2] ? Number.parseInt(match[2]) : 1
+      const addStart = Number.parseInt(match[3])
+      const addNumber = match[4] ? Number.parseInt(match[4]) : 1
+
+      for (let j = 0; j < remNumber; j++) {
+        removed.push(remStart + j)
+      }
+      for (let j = 0; j < addNumber; j++) {
+        added.push(addStart + j)
+      }
+    }
+  }
+
+  return { added, removed }
+}
+
+/**
+ * Gets the base and head refs from the github context.
+ *
+ * The base ref will be:
+ * - The base branch of the pull request, or
+ * - The commit before the push
+ *
+ * @returns {object} { base, head, owner, repo }
+ */
+function getRefs() {
+  const { payload, eventName } = github.context
+
+  core.startGroup('getRefs: payload')
+  core.debug(`getRefs: payload=${JSON.stringify(payload, null, 2)}`)
+  core.endGroup()
+  core.debug(`getRefs: eventName=${JSON.stringify(eventName)}`)
+
+  const {
+    repository: {
+      name: repo,
+      owner: { login: owner }
+    },
+    before,
+    after: head
+  } = payload
+
+  core.debug(`getRefs: repo=${JSON.stringify(repo)}`)
+  core.debug(`getRefs: owner=${JSON.stringify(owner)}`)
+  core.debug(`getRefs: before=${JSON.stringify(before)}`)
+  core.debug(`getRefs: head=${JSON.stringify(head)}`)
+
+  let base
+  if (eventName === 'pull_request') {
+    base = payload.pull_request.base.sha
+  } else if (eventName === 'push') {
+    base = before
+  } else {
+    throw new Error('The triggering event must be "push" or "pull_request"')
+  }
+
+  core.debug(`getRefs: base=${JSON.stringify(base)}`)
+
+  return { base, head, owner, repo }
+}
+
+/**
+ * Executes a shell command and resolves to the output
+ * once the command finishes.
+ *
+ * By default, rejects if there is data on stderr or if the exit code is not zero.
+ * See more options at
+ * <https://github.com/actions/toolkit/blob/main/packages/exec/src/interfaces.ts>
+ *
+ * @param {string} command
+ * @param {string[]} args
+ * @param {object} options
+ * @returns {object} { stdout: string, stderr: string, code: number }
+ */
+async function execAsync(command, args = [], options = {}) {
+  const errArray = []
+  const outArray = []
+  let code = -1
+
+  core.debug(`execAsync: command=${command}`)
+  core.debug(`execAsync: args=${JSON.stringify(args, null, 2)}`)
+  core.debug(`execAsync: (input) options=${JSON.stringify(options, null, 2)}`)
+
+  options = {
+    failOnStdErr: true,
+    silent: true,
+    ...options,
+    listeners: {
+      ...(options.listeners ? options.listeners : {}),
+      // Use stdout instead of stdline https://github.com/actions/toolkit/issues/749
+      stdout: data => outArray.push(data),
+      stderr: data => errArray.push(data)
+    }
+  }
+
+  try {
+    code = await exec.exec(command, args, options)
+  } catch (e) {
+    e.stderr = errArray.join('')
+    e.stdout = outArray.join('')
+    core.error(`execAsync: stdout=${e.stdout}`)
+    core.error(`execAsync: stderr=${e.stderr}`)
+    throw e
+  }
+
+  return { stdout: outArray.join(''), stderr: errArray.join(''), code }
+}
